3b6c91135ddb2c7026aec5191b23290c97dd7834
colin schmidt <colin.schm@gmail.com>
2021-08-09 16:59:30 +0200

    Creation controller and model for orders2.0

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,33 @@
+class OrdersController < ApplicationController
+  before_action :set_orders, only: %i[ show create ]
+
+  def show
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+
+    @order.wizard = @wizard
+
+    if @order.save
+      redirect_to wizard_path(@wizard)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def orders
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:amount, :start_date, :end_date)
+  end
+
+end
