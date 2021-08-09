a9039e7b04884dfde1ac1ecff865001072fab584
colin schmidt <colin.schm@gmail.com>
2021-08-09 16:56:18 +0200

    Creation controller and model for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :wizard
+  belongs_to :user
+
+  validates :amount, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
