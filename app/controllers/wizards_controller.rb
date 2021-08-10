ecb7f33b5150c3502d26cdadc022d9163b1e411a
hadrien morand <hadrien.morand@ehl.ch>
2021-08-10 15:03:02 +0200

    corrections

--- app/controllers/wizards_controller.rb
+++ app/controllers/wizards_controller.rb
@@ -1,6 +1,6 @@
 class WizardsController < ApplicationController
 
-  before_action :set_wizard, only [:show, :edit, :update, :destroy]
+  before_action :set_wizard, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only:[:index, :show]
 
   def index
