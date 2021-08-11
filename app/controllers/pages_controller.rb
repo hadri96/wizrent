9ec544d7fb2fc3bf9bbfc4576e6844e9f65158de
colin schmidt <colin.schm@gmail.com>
2021-08-11 15:19:43 +0200

    Cards done home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @wizards = Wizard.all
+    @wizards = Wizard.all.limit(9)
   end
 end
